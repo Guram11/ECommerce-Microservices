25aabc09c1b54157c79bfa4a3d9f458210f32004
Guram11 <guramchubinidze11@gmail.com>
2024-09-30 14:54:54 +0400

    Uploading source code

---
+++ src/Services/Catalog/Catalog.API/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using MediatR;
+global using Carter;
+global using Mapster;
+global using BuildingBlocks.CQRS;
+global using Catalog.API.Models;
+global using Catalog.API.Exceptions;
+global using Marten;
+global using Marten.Pagination;
+global using FluentValidation;
